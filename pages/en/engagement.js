175be4d90bd816b531f2f4dbef5499a7ea0c96cb
Fatih Gozenc <fatihgozenc@gmail.com>
2020-01-22 19:30:54 +0100

    engagement fixed

--- pages/en/engagement.js
+++ pages/en/engagement.js
@@ -1,10 +1,10 @@
-import { API_HOST } from '../config';
-import Engagement from '../components/templates/engagement';
+import { API_HOST } from '../../config';
+import Engagement from '../../components/templates/engagement';
 
 const EngagementPage = ({ data }) => <Engagement data={data} />
 
 EngagementPage.getInitialProps = async () => {
-	const res = await fetch(`${API_HOST}de/Engagement`)
+	const res = await fetch(`${API_HOST}en/Engagement`)
 	const json = await res.json()
 	return { data: json }
 }
